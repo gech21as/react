73ec2722e76d142cf906860f34cc555a9fe0d221
getahun <getahunassefa277@gmail.com>
2025-10-18 11:14:48 +0300

    initial commit

---
+++ Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-img" src="https://via.placeholder.com/150" alt="profile picture" />
+            <h2 className="card-name">Gech's trial</h2>
+            <p className="card-description">now i am trying to learn react course</p>
+        </div>
+        
+    );
+}
+
+export default Card;
